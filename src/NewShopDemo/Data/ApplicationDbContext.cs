d522ff09728d0aa83c7c03a353d03d42963d5225
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-10-13 20:35:20 -0300

    feat: Add OnConfiguring method to ApplicationDbContext

---
+++ src/NewShopDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace NewShopDemo.Data
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        // public DbSet<Customer> Customers { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options: options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Database=NewShopDemo;User=SA;Password=Password123!;TrustServerCertificate=True;MultipleActiveResultSets=true";
+            optionsBuilder.UseSqlServer(connectionString: connectionString);
+        }
+    }
+}
