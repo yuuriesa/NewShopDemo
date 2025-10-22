210ffb34a04113dd449aff12826b3aa37903ad45
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-10-22 16:08:16 -0300

    feat: add private properties (Customer) class

--- src/NewShopDemo/models/Customer.cs
+++ src/NewShopDemo/models/Customer.cs
@@ -1,7 +1,15 @@
+using System.Net.Sockets;
+
 namespace NewShopDemo.Models
 {
     public class Customer
     {
-        
+        // private properties
+        private string _firstName;
+        private string _lastName;
+        private string _emailAddress;
+        private DateOnly _dateOfBirth;
+        //private ICollection<Address> _addresses = new List<Address>();
+        private bool _isValid { get; set; } = false;
     }
 }
