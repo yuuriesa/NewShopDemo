e061b7a5f8b53e616b8b0b3bfedde3ee314f9f70
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-10-22 16:20:36 -0300

    feat add public properties (Customer) class

--- src/NewShopDemo/models/Customer.cs
+++ src/NewShopDemo/models/Customer.cs
@@ -11,5 +11,15 @@ namespace NewShopDemo.Models
         private DateOnly _dateOfBirth;
         //private ICollection<Address> _addresses = new List<Address>();
         private bool _isValid { get; set; } = false;
+
+        // public properties
+        public int CustomerId { get; private set; }
+        public string FirstName => _firstName;
+        public string LastName => _lastName;
+        public string EmailAddress => _emailAddress;
+        public DateOnly DateOfBirth => _dateOfBirth;
+        //public ICollection<Address> Addresses => _addresses;
+        //public ICollection<Order> Orders { get; set; } = new List<Order>();
+        public string ErrorMessageIfIsNotValid { get; private set; } = string.Empty;
     }
 }
