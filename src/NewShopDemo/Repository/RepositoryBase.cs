0b83fbd7e16fc816cc5c86382e0547e9147730f4
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-10-23 21:42:30 -0300

    feat: Add constructor to RepositoryBase (class)

--- src/NewShopDemo/Repository/RepositoryBase.cs
+++ src/NewShopDemo/Repository/RepositoryBase.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using NewShopDemo.Data;
+
 namespace NewShopDemo.Repository
 {
     public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
     {
-        
+        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<TEntity> _dbSetEntity;
+
+        public RepositoryBase(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSetEntity = _dbContext.Set<TEntity>();
+        }
     }
 }
