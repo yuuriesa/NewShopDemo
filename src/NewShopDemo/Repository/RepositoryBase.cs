7ddd1584c328398250f62a6b751ff081982c10b0
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-10-26 11:12:16 -0300

    feat: Implemented interface to RepositoryBase (class)

--- src/NewShopDemo/Repository/RepositoryBase.cs
+++ src/NewShopDemo/Repository/RepositoryBase.cs
@@ -14,9 +14,34 @@ namespace NewShopDemo.Repository
             _dbSetEntity = _dbContext.Set<TEntity>();
         }
 
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public void SaveChanges()
         {
             _dbContext.SaveChanges();
+        }
+
+        public void Update(int id, TEntity entity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
