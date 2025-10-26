a251b83bf0e76eaad829680077fa16bd3bba1dc0
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-10-26 11:06:53 -0300

    feat: add AddRange(method) to IRepositoryBase (interface)

--- src/NewShopDemo/Repository/IRepositoryBase.cs
+++ src/NewShopDemo/Repository/IRepositoryBase.cs
@@ -5,5 +5,7 @@ namespace NewShopDemo.Repository
         // public IQueryable<TEntity> GetAll(PaginationFilter paginationFilter);   
         public TEntity GetById(int id);
         public void Add(TEntity entity);
+        public void AddRange(IEnumerable<TEntity> entities);
+        
     }
 }
