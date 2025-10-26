ec31df49ebc635346cec7437080c2c600bfe47c3
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-10-26 11:04:20 -0300

    feat: add GetById to IRepositoryBase (interface)

--- src/NewShopDemo/Repository/IRepositoryBase.cs
+++ src/NewShopDemo/Repository/IRepositoryBase.cs
@@ -3,5 +3,6 @@ namespace NewShopDemo.Repository
     public interface IRepositoryBase<TEntity> where TEntity : class
     {
         // public IQueryable<TEntity> GetAll(PaginationFilter paginationFilter);   
+        public TEntity GetById(int id);
     }
 }
