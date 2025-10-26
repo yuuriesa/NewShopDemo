4e0c4a7de0dcab84d91ab4cdf012d910161a1b52
yurisalgado21 <yuri.salgado.ft@gmail.com>
2025-10-26 11:01:43 -0300

    feat: add GetAll to IRepositoryBase (interface)

--- src/NewShopDemo/Repository/IRepositoryBase.cs
+++ src/NewShopDemo/Repository/IRepositoryBase.cs
@@ -2,6 +2,6 @@ namespace NewShopDemo.Repository
 {
     public interface IRepositoryBase<TEntity> where TEntity : class
     {
-        
+        // public IQueryable<TEntity> GetAll(PaginationFilter paginationFilter);   
     }
 }
